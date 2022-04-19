134945faa47328a50de3fbf7b849ac689a3b2300
unknown <hotien2107@gmail.com>
2022-04-19 15:16:05 +0700

    wrap provider

--- src/app.tsx
+++ src/app.tsx
@@ -6,6 +6,8 @@ import RightContent from '@/components/RightContent';
 import Footer from '@/components/Footer';
 import { currentUser as queryCurrentUser } from './services/ant-design-pro/api';
 import { BookOutlined, LinkOutlined } from '@ant-design/icons';
+import { Provider } from 'react-redux';
+import store from './stores';
 
 const isDev = process.env.NODE_ENV === 'development';
 const loginPath = '/user/login';
@@ -50,6 +52,9 @@ export async function getInitialState(): Promise<{
 // ProLayout 支持的api https://procomponents.ant.design/components/layout
 export const layout: RunTimeLayoutConfig = ({ initialState }) => {
   return {
+    childrenRender: (x) => {
+      return <Provider store={store}>{x}</Provider>;
+    },
     rightContentRender: () => <RightContent />,
     disableContentMargin: false,
     footerRender: () => <Footer />,
