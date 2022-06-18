aef1e7ed7913d1d1e653a1d57515db173abdfba3
Tien <hotien2107@gmail.com>
2022-06-18 17:00:46 +0700

    fix bug update category model

--- src/pages/model/table-model/components/UpdateForm.tsx
+++ src/pages/model/table-model/components/UpdateForm.tsx
@@ -1,30 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { ModalForm, ProFormText, ProFormSelect } from '@ant-design/pro-form';
 import { Button, message, Upload, Form } from 'antd';
-import type { InputForm, SelectedInterface } from "../data";
-import {  UploadOutlined } from '@ant-design/icons';
+import type { InputForm, SelectedInterface } from '../data';
+import { UploadOutlined } from '@ant-design/icons';
 import CategoryListProxy from '@/services/proxy/item-categories/get-item-categories';
 import axios from 'axios';
 import { ItemInterface } from '@/types/item';
 import { ProxyStatusEnum } from '@/types/http/proxy/ProxyStatus';
 
-
-
 type UpdateFormProps = {
   modalVisible: boolean;
   handleModalVisible: (value: boolean | ((prevVar: boolean) => boolean)) => void;
-  onSubmit:(values:InputForm, id: number)=>void;
+  onSubmit: (values: InputForm, id: number) => void;
   currentItem: ItemInterface;
 };
 
 const UpdateForm: React.FC<UpdateFormProps> = (props) => {
-  const [form] = Form.useForm()
+  const [form] = Form.useForm();
   const { modalVisible, onSubmit, handleModalVisible, currentItem } = props;
   const [isModel, setIsModel] = useState<string>(currentItem.modelPath);
   const [isImage, setIsImage] = useState<string>(currentItem.image);
   const [categoryList, setCategoryList] = useState<SelectedInterface[]>();
 
- useEffect(() => {
+  useEffect(() => {
     CategoryListProxy({})
       .then((res) => {
         if (res.status === ProxyStatusEnum.FAIL) {
@@ -38,36 +36,39 @@ const UpdateForm: React.FC<UpdateFormProps> = (props) => {
             let tmp: SelectedInterface = {
               value: item.id.toString(),
               label: item.name,
-            }
+            };
             list.push(tmp);
-          })
+          });
           setCategoryList(list);
         }
       })
       .catch((err) => {
         message.error("Don't load category list");
       });
-      form.resetFields();
-  }, [currentItem])
+    form.resetFields();
+  }, [currentItem]);
 
   return (
     <ModalForm
-    form={form}
-    title="Update model"
-    width="400px"
-    visible={modalVisible}
-    onVisibleChange={handleModalVisible}
-    onFinish={async (value) => {
-      const tmp:InputForm ={
-        name: value.name.trim()==""?"":value.name,
-        categoryId: Number.parseInt(value.category)==NaN?Number.parseInt(value.category):currentItem?.category?.id,
-        image: isImage,
-        modelPath: isModel,
-      }
-      onSubmit(tmp,currentItem?.id);
-    }}
-  >
-        <ProFormText
+      form={form}
+      title="Update model"
+      width="400px"
+      visible={modalVisible}
+      onVisibleChange={handleModalVisible}
+      onFinish={async (value) => {
+        const tmp: InputForm = {
+          name: value.name.trim() == '' ? '' : value.name,
+          categoryId:
+            Number.parseInt(value.category) !== NaN
+              ? Number.parseInt(value.category)
+              : currentItem?.category?.id,
+          image: isImage,
+          modelPath: isModel,
+        };
+        onSubmit(tmp, currentItem?.id);
+      }}
+    >
+      <ProFormText
         rules={[
           {
             required: true,
@@ -89,7 +90,7 @@ const UpdateForm: React.FC<UpdateFormProps> = (props) => {
         rules={[{ required: true, message: 'Please select your country!' }]}
       />
       <Upload
-        style={{ marginBottom: "1rem" }}
+        style={{ marginBottom: '1rem' }}
         name="model"
         accept=".glb, .gltf"
         listType="picture"
@@ -123,12 +124,12 @@ const UpdateForm: React.FC<UpdateFormProps> = (props) => {
                   return;
                 }
               });
-          } catch (err) {
-          }
+          } catch (err) {}
         }}
       >
-        <Button style={{ marginBottom: "1rem" }}
-          icon={<UploadOutlined />}>Click to upload model</Button>
+        <Button style={{ marginBottom: '1rem' }} icon={<UploadOutlined />}>
+          Click to upload model
+        </Button>
       </Upload>
       <Upload
         name="img"
@@ -159,14 +160,13 @@ const UpdateForm: React.FC<UpdateFormProps> = (props) => {
                   return;
                 }
               });
-          } catch (err) {
-          }
+          } catch (err) {}
         }}
         accept=".png,.jpg,.jpeg"
       >
         <Button icon={<UploadOutlined />}>Click to upload image</Button>
       </Upload>
-  </ModalForm>
+    </ModalForm>
   );
 };
 
